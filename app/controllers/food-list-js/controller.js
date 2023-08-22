3534c27678389a13a09ef0e1b0ca3267673dab4c
DuyNguyenlk22 <nguyenduylk202@gmail.com>
2023-08-22 19:56:39 +0700

    start

---
+++ app/controllers/food-list-js/controller.js
@@ -0,0 +1,83 @@
+export let BASE_URL = "https://64d6fadf2a017531bc12e6f4.mockapi.io/food";
+
+const LOAI = true;
+const CON_HET = true;
+const CON_MON = true;
+export let render = (list) => {
+  let content = "";
+  list.reverse().forEach((item) => {
+    let { ma, ten, loai, gia, khuyenMai, tinhTrang, img, desc } = item;
+    let listTr = /*html*/ `
+    <tr>
+      <td>${ma}</td>
+      <td>${ten}</td>
+      <td>${loai == LOAI ? "Chay" : "Mặn"}</td>
+      <td>${gia}</td>
+      <td>${khuyenMai}</td>
+      <td>0</td>
+      <td>${tinhTrang == CON_MON ? "1" : "0"}</td>
+      <td><button class="btn btn-info"onclick="editFood(${ma})">Sửa</button>
+      <button class="btn btn-danger" onclick="deleteFood(${ma})">Xoá</button>
+      </td>
+    </tr>
+    `;
+    content += listTr;
+  });
+  document.getElementById("tbodyFood").innerHTML = content;
+};
+
+export let fetchFoodList = () => {
+  axios
+    .get(BASE_URL)
+    .then((res) => {
+      console.log(res);
+      render(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export let showMessage = (message, isSuccess = true) => {
+  Toastify({
+    text: message,
+    className: "info",
+    style: {
+      background: isSuccess
+        ? "linear-gradient(to right, #00b09b, #96c93d)"
+        : "red",
+    },
+  }).showToast();
+};
+
+export let getDataForm = () => {
+  let ma = document.getElementById("foodID").value;
+  let ten = document.getElementById("tenMon").value;
+  let loai = document.getElementById("loai").value;
+  let gia = document.getElementById("giaMon").value;
+  let khuyenMai = document.getElementById("khuyenMai").value;
+  let tinhTrang = document.getElementById("tinhTrang").value;
+  let img = document.getElementById("hinhMon").value;
+  let desc = document.getElementById("moTa").value;
+  return {
+    ma,
+    ten,
+    loai,
+    gia,
+    khuyenMai,
+    tinhTrang,
+    img,
+    desc,
+  };
+};
+export let showDataForm = (data) => {
+  let { ma, ten, loai, gia, khuyenMai, tinhTrang, img, desc } = data;
+  document.getElementById("foodID").value = ma;
+  document.getElementById("tenMon").value = ten;
+  document.getElementById("loai").value = loai == LOAI ? "loai1" : "loai2";
+  document.getElementById("giaMon").value = gia;
+  document.getElementById("khuyenMai").value = khuyenMai;
+  document.getElementById("tinhTrang").value = tinhTrang == CON_HET ? "1" : "0";
+  document.getElementById("hinhMon").value = img;
+  document.getElementById("moTa").value = desc;
+};
---
+++ app/controllers/food-list-js/main.js
@@ -0,0 +1,67 @@
+import {
+  BASE_URL,
+  fetchFoodList,
+  getDataForm,
+  showDataForm,
+  showMessage,
+} from "./controller.js";
+
+fetchFoodList();
+
+window.deleteFood = (id) => {
+  console.log("🚀 ~ file: main.js:8 ~ window.deleteFood ~ id:", id);
+  axios
+    .delete(`${BASE_URL}/${id}`)
+    .then((res) => {
+      console.log(res);
+      showMessage("Xoá thành công");
+      fetchFoodList();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+window.addFood = () => {
+  let data = getDataForm();
+  axios
+    .post(BASE_URL, data)
+    .then((res) => {
+      console.log(res);
+      showMessage("Thêm thành công");
+      $("#exampleModal").modal("hide");
+      fetchFoodList();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+window.editFood = (id) => {
+  console.log("🚀 ~ file: main.js:40 ~ id:", id);
+  axios
+    .get(`${BASE_URL}/${id}`)
+    .then((res) => {
+      console.log(res);
+      $("#exampleModal").modal("show");
+      showDataForm(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+window.updateFood = () => {
+  let data = getDataForm();
+  axios
+    .put(`${BASE_URL}/${data.ma}`, data)
+    .then((res) => {
+      console.log(res);
+      showMessage("Cập nhật thành công");
+      $("#exampleModal").modal("hide");
+      fetchFoodList();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
